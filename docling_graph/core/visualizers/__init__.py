c5d77cc5ec27437b115a8374593a8a65d2e4160f
Ayoub El Bouchtili <ayoub.elbouchtili@fr.ibm.com>
2025-10-26 10:12:05 +0100

    fix(core): add init scripts to fix imports issues

---
+++ docling_graph/core/visualizers/__init__.py
@@ -0,0 +1,4 @@
+from .static_visualizer import StaticVisualizer
+from .interactive_visualizer import InteractiveVisualizer
+
+__all__ = ["StaticVisualizer", "InteractiveVisualizer"]
---
+++ docling_graph/extractors/__init__.py
@@ -0,0 +1,4 @@
+from .strategies.one_to_one import OneToOneStrategy
+from .strategies.many_to_one import ManyToOneStrategy
+
+__all__ = ["OneToOneStrategy", "ManyToOneStrategy"]
